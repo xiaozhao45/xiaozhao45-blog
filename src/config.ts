19c755aff91735f7818d8f353d2ca68fc27602e5
xiaozhao45 <a1823127@outlook.com>
2025-11-14 12:54:42 +0800

    Fixed

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "♪Welcome to my world♪",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 265, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 360, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
--- src/content/posts/Hello,world!.md
+++ src/content/posts/Hello,world!.md
@@ -3,8 +3,7 @@ title: 你好世界！
 published: 2025-11-13
 tags: [Blogging]
 category: Default
-draft: true
+draft: false
 ---
 
 你好世界！
-test2
